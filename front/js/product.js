d44c6ca2b3343457fbb330a369a0f4c47e253650
yanguel <yanguelcornillet@yahoo.com>
2022-08-17 00:35:17 +0200

    Recherche par ID de la page product HTML

--- front/js/product.js
+++ front/js/product.js
@@ -1,8 +1,13 @@
+let str = window.location;
+let url = new URL(str);
+let search_params = new URLSearchParams(url.search);
+let iditem = search_params.get("id");
+
 // Récupération des données concernant les canapés.
-let urlBase = fetch(" http://localhost:3000/api/products")
+let urlBase = fetch(" http://localhost:3000/api/products/" + iditem)
   .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
+  .then((canape) => {
+    console.log(canape);
 
     /* <article>
             <div class="item__img">
@@ -12,58 +17,40 @@ let urlBase = fetch(" http://localhost:3000/api/products")
 
     //Utilisation du parent commun Article
     const container = document.querySelector("article");
-
-    // Création de la boucle for afin de parcourir le tableau récupéré précédemment.
-    for (let i = 0; i < data.length; i++) {
-      var str = "http://localhost:3000/api/products";
-      var url = new URL(str);
-      var search_params = new URLSearchParams(url.search);
-      if (search_params.has("_id")) {
-        var iditem = search_params.get("_id");
-        console.log(iditem);
-      }
-
-      //Création de la variable représentant la class "item__img"
-      const classimg = document.getElementsByClassName("item__img");
-
-      // Création de l'élément img + récuperation de l'image + utilisation du alt
-      const linkelt = document.createElement("img");
-      linkelt.src = data[i].imageUrl;
-      linkelt.alt = data[i].name;
-
-      // Recherche de l'ID
-      linkelt.href = "./product.html?id=" + data[i]._id;
-
-      //Création des enfants
-      classimg[0].appendChild(linkelt);
-
-      // Rajout du nom du produit
-      const classname = document.getElementById("title");
-      classname.innerHTML = data[i].name;
-
-
-      // Rajout du prix en fonction de l'id 
-      const classprice = document.getElementById("price");
-      classprice.innerHTML = data[i].price;
-
-      // Rajout de la description en fonction de l'id
-      const classdescription = document.getElementById("description");
-      classdescription.innerHTML = data[i].description; 
-
-    }
-  });
-
-  // Rajout des options "Vert" et "Blanc" hors de la boucle !
-  var newoption = document.getElementById('colors');
-  var options = [ 'vert', 'blanc'];
-
-  options.forEach(function(element, key) {
-    if (element == 'vert') {
-      newoption[newoption.options.length] = new Option(element, newoption.options.length, false, false);
-    }
-    if (element == 'blanc') {
-      newoption[newoption.options.length] = new Option(element, newoption.options.length, true, false); 
-      
-    }
     
+
+        //Création de la variable représentant la class "item__img"
+        const classimg = document.getElementsByClassName("item__img");
+
+        // Création de l'élément img + récuperation de l'image + utilisation du alt
+        const linkelt = document.createElement("img");
+        linkelt.src = canape.imageUrl;
+        linkelt.alt = canape.name;
+
+        // Recherche de l'ID
+        linkelt.href = "./product.html?id=" + canape._id;
+
+        //Création des enfants
+        classimg[0].appendChild(linkelt);
+
+        // Rajout du nom du produit
+        const classname = document.getElementById("title");
+        classname.innerHTML = canape.name;
+
+
+        // Rajout du prix en fonction de l'id 
+        const classprice = document.getElementById("price");
+        classprice.innerHTML = canape.price;
+
+        // Rajout de la description en fonction de l'id
+        const classdescription = document.getElementById("description");
+        classdescription.innerHTML = canape.description; 
+        // Rajout des options "Vert" et "Blanc" hors de la boucle !
+        let newoption = document.getElementById('colors');
+        let options = canape.colors;
+      
+
+        options.forEach(function(element, _) {
+            newoption[newoption.options.length] = new Option(element, newoption.options.length, false, false);
+        });
   });
