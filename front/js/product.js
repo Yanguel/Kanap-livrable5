e43e4a4fc68eb84b9bbc59950d410d5b60b08abe
yanguel <yanguelcornillet@yahoo.com>
2022-08-16 18:46:13 +0200

    Rajout du nom du produit sur la page de l'article (product.html)
    Rajout du nom du produit sur la page de l'article (product.html)

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,5 @@
 // Récupération des données concernant les canapés.
-let urlbase = fetch(" http://localhost:3000/api/products")
+let urlBase = fetch(" http://localhost:3000/api/products")
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
@@ -10,27 +10,39 @@ let urlbase = fetch(" http://localhost:3000/api/products")
             </div> 
     */
 
-                  //Utilisation du parent commun Article
+    //Utilisation du parent commun Article
     const container = document.querySelector("article");
-    
+
     // Création de la boucle for afin de parcourir le tableau récupéré précédemment.
     for (let i = 0; i < data.length; i++) {
-        
-        const classimg = document.getElementsByClassName("item__img");
-        
-        
 
-        // Création de l'élément img + récuperation de l'image + utilisation du alt 
-        const linkelt = document.createElement("img");
-        linkelt.src = data[i].imageUrl;
-        linkelt.alt = data[i].name;
-        
-        // Recherche de l'ID
-        linkelt.href = "./product.html?id=" + data[i]._id;
+        var str = "http://localhost:3000/api/products";
+        var url = new URL(str);
+        var search_params = new URLSearchParams(url.search); 
+        if(search_params.has('_id')) {
+          var iditem = search_params.get('_id');
+          console.log(iditem)
+        }
+
+      //Création de la variable représentant la class "item__img"
+      const classimg = document.getElementsByClassName("item__img");
+
+      // Création de l'élément img + récuperation de l'image + utilisation du alt
+      const linkelt = document.createElement("img");
+      linkelt.src = data[i].imageUrl;
+      linkelt.alt = data[i].name;
+
+      // Recherche de l'ID
+      linkelt.href = "./product.html?id=" + data[i]._id;
+
+      //Création des enfants
+      classimg[0].appendChild(linkelt);
 
 
-      //Création des enfants 
-        classimg[0].appendChild(linkelt)
 
+      const classname = document.getElementById("title");
+      classname.innerHTML = data[i].name;
+
+      item__content.appendChild(classname)
     }
-});
+  });
