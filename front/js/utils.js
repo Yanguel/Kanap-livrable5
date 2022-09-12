9b22fa169f39fc22d87a9a9fe2b4535c45117642
yanguel <yanguelcornillet@yahoo.com>
2022-09-12 11:21:12 +0200

    Rajout de commentaire sur les fonction existante. Utils.js

--- front/js/utils.js
+++ front/js/utils.js
@@ -1,17 +1,12 @@
 /* eslint-disable */
 
-function calculTotalNbElementsPanier(panier){
-    let total = 0
-    panier.forEach(product => {
-      //console.log(product);
-      total = total + parseInt(product.quantity);
-    });
-    return total;
-} 
+// Fonction qui permet de vérifier que la saisi ne contient pas de chiffre. (Regex)
 function regexSansChiffre(veriftext) {
   var exp = new RegExp("^[a-zA-Z]*$","g")
   return exp.test(veriftext);
 }
+
+// Fonction pour donne l'instruction si le prenom à un chiffre.
 function checkPrenom(textNomPrenom) {
   if (regexSansChiffre(textNomPrenom)) {
     return textNomPrenom;
@@ -20,6 +15,7 @@ function checkPrenom(textNomPrenom) {
     return false;
   }
 }
+// Fonction pour donne l'instruction si le nom à un chiffre.
 function checkNom(textNomPrenom) {
   if (regexSansChiffre(textNomPrenom)) {
     return textNomPrenom;
@@ -28,11 +24,13 @@ function checkNom(textNomPrenom) {
     return false;
   }
 }
+// Fonction qui vérifie si l'email correspond au attente. (Regex)
 function validerEmail(email) {
   const regle =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return regle.test(email);
 }
+//Fonction qui donne l'instruction si l'email ne correspond pas à la regex.
 function checkEmail(textEmail) {
   if (validerEmail(textEmail)) {
     return textEmail;
@@ -41,15 +39,16 @@ function checkEmail(textEmail) {
     return false;
   }
 }
-
-function displayTotal(produits) {
-  const total = calculTotal(produits);
-  document.querySelector("#totalPrice").textContent = total;
-  const totalElementsPanier = calculTotalNbElementsPanier(produits);
-  document.querySelector("#totalQuantity").textContent = totalElementsPanier;
-}
-
-// je veux calculer le total des articles de mon panier
+// Fonction qui permet de calculer le nombre d'élément dans un tableau.
+function calculTotalNbElementsPanier(panier){
+    let total = 0
+    panier.forEach(product => {
+      //console.log(product);
+      total = total + parseInt(product.quantity);
+    });
+    return total;
+} 
+// Permet de calculer le prix total des articles de mon panier
 function calculTotal(produits) {
   // 1 - definir un total a 0
   let total = 0;
@@ -63,5 +62,13 @@ function calculTotal(produits) {
   // en fin de boucle renvoyer le total
   return total;
 }
+//Fonction qui permet d'afficher le prix total du panier et la quantité.
+function displayTotal(produits) {
+  const total = calculTotal(produits);
+  document.querySelector("#totalPrice").textContent = total;
+  const totalElementsPanier = calculTotalNbElementsPanier(produits);
+  document.querySelector("#totalQuantity").textContent = totalElementsPanier;
+}
+
 
 export {calculTotal, calculTotalNbElementsPanier, checkEmail, displayTotal, validerEmail, regexSansChiffre, checkNom, checkPrenom}
